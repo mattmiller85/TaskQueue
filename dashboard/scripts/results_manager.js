3925a812b53456a27851e0ff74c6abe678c367df
Matt Miller <mattmiller85@gmail.com>
2017-02-01 15:41:36 -0500

    uncomment

--- dashboard/scripts/results_manager.js
+++ dashboard/scripts/results_manager.js
@@ -1,21 +1,18 @@
 /*jslint node: true */
 'use strict';
 
-function ResultsManager() {
+function ResultsManager(options) {
   var self = this;
-  //self.ws = new WebSocket("http://localhost:876");
-
+  self.ws = new WebSocket("http://localhost:876");
+  
   
 }
 
 ResultsManager.prototype.askForLatestResults = function(numResults){
-    alert("here");
-    // var self = this;
-    // if(numResults == null)
-    //     numResults = 50;
-    // self.ws.send({ "type": "give_latest", "params": { "count": numResults } });
+    var self = this;
+    if(numResults == null)
+        numResults = 50;
+    self.ws.send({ "type": "give_latest", "params": { "count": numResults } });
 };
-
-
 
 module.exports = ResultsManager;
